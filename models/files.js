195f908a1e523d0cc675ccb4319379718e2159b0
orc1nusorca <Sonia.Shatova@yandex.ru>
2025-04-07 22:55:27 +0300

    Delete models directory

--- models/files.js
+++
@@ -1,2 +0,0 @@
-const files = [];
-module.exports = { files };
--- models/users.js
+++
@@ -1,5 +0,0 @@
-const users = [
-    { id: 1, email: 'test@test.com', password: 'password', firstName: 'Test', lastName: 'User' },
-];
-
-module.exports = { users };
